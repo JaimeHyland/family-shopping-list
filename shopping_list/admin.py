5ce3b658e9f114a31d157eb53fa529072081abf7
Jaime Hyland <james.hyland@iubh.de>
2024-05-01 11:14:21 +0000

    Dev: #26, 27; improve format of homepage (shopping list).

--- shopping_list/admin.py
+++ shopping_list/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Shop, Category, Product, List_item
 
+
 # Register your models here.
 admin.site.register(Shop)
 admin.site.register(Category)
--- shopping_list/views.py
+++ shopping_list/views.py
@@ -10,3 +10,11 @@ class ItemListView(generic.ListView):
     template_name = 'shopping_list.html'
     paginate_by = 12
     current_list = List_item.objects.all()
+
+
+    # change the name of the very abstract and general "object_list" to 
+    # the much more descriptive "shopping_list"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['shopping_list'] = context['object_list']
+        return context
